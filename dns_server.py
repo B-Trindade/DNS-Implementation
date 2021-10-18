70459a9492cb81c552e6465e846558c608102412
Breno Trindade <bttostes@dcc.ufrj.br>
2021-10-18 09:42:42 -0300

    Documentation update
    Class DNSserver documentation changed

--- dns_server.py
+++ dns_server.py
@@ -23,10 +23,8 @@ def threaded(fn):
 
 class DNSserver():
     '''
-    A server must register either a Host process or a Server process:
-        - a HOST will provide a pair (hostname, PID)
-        - a SERVER will provide a pair (dns, PID)
-    The lookup after no ENTRY is found is implemented by the RESOLVER.
+    A server must register either a Host process or a Server process.
+    The lookup is found is implemented by the RESOLVER.
     '''
 
     def __init__(self) -> None:
